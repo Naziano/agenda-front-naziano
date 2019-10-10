7942b9c2954ff429e9d8433e83a60a5d011c47e5
NAZIANO FELIX DE ALMEIDA JUNIOR <a9187448@rn.senac.br>
2019-10-09 21:22:56 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class Contato {
+  id: number;
+  nome: string;
+  fone: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  ultimoId = 3;
+  contato = new Contato();
+
+  contatos = [];
+
+
+  salvar(formContato: NgForm) {
+    this.contato.id = ++this.ultimoId;
+    this.contato.nome = formContato.value.nome;
+    this.contato.fone = formContato.value.fone;
+
+    this.contatos.push(this.contato);
+
+    this.contato = new Contato();
+  }
+
+  editar(id: number) {
+    for (const c of this.contatos) {
+      console.log(c);
+    }
+  }
+
+}
